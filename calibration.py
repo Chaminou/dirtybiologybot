93dcaa3f71f8729e0848bed31116666c13e664d2
Pierre <pierre@chaminade.fr>
2021-11-17 17:13:51 +0100

    initial commit mdr

---
+++ calibration.py
@@ -0,0 +1,101 @@
+import pyautogui
+import time
+import sys
+from canard import a
+
+def get_position() :
+    pos = pyautogui.position()
+    return pos[0], pos[1]
+
+print("Bienvenue aux rickrollers")
+time.sleep(1)
+
+print("""Rendez vous sur la page d'édition du drapeau puis reclickez sur ce programme et appuyez sur ENTRER""")
+input()
+time.sleep(1)
+
+print("Calibration")
+time.sleep(1)
+
+print("Mettez votre curseur (sans cliquer) au dessus d'un endroit sans interaction (bord gauche recommandé) du site et appuyez sur ENTRER")
+input()
+neutral_coord = get_position()
+print(neutral_coord)
+pyautogui.moveTo(neutral_coord)
+pyautogui.click()
+pyautogui.press('f5')
+print("rafraichissement en cours et recliquez sur ce programme")
+time.sleep(10)
+print('appuyez sur ENTRER')
+input()
+
+
+print("Mettez votre curseur (sans cliquer) au dessus du bouton 'Editer le drapeau' et appuyez sur ENTRER")
+input()
+edit_coord = get_position()
+print(edit_coord)
+time.sleep(1)
+
+print("Attention, phase de calibration en cours, veuillez ne toucher à rien pendant 20 secondes")
+def setup() :
+    global neutral_coord
+    global edit_coord
+
+    pyautogui.moveTo(neutral_coord)
+    pyautogui.click()
+    time.sleep(1)
+    pyautogui.press('f5')
+    time.sleep(10)
+    pyautogui.moveTo(edit_coord)
+    pyautogui.click()
+    time.sleep(10)
+    pyautogui.moveTo(neutral_coord)
+    pyautogui.scroll(-2)
+setup()
+print("Calibration 1/2 terminée")
+time.sleep(1)
+print("reclickez sur le programme et appuyez sur ENTRER")
+input()
+
+print("Mettez votre curseur (sans cliquer) au dessus de la boite de dialogue pour le X du pixel et appuyez sur ENTRER")
+input()
+x_coord = get_position()
+print(x_coord)
+time.sleep(1)
+
+print("Mettez votre curseur (sans cliquer) au dessus de la boite de dialogue pour le Y du pixel et appuyez sur ENTRER")
+input()
+y_coord = get_position()
+print(y_coord)
+time.sleep(1)
+
+print("Mettez votre curseur (sans cliquer) au dessus de la boite de dialogue pour la couleur du pixel et appuyez sur ENTRER")
+input()
+color_coord = get_position()
+print(color_coord)
+time.sleep(1)
+
+print("Mettez votre curseur (sans cliquer) au dessus du bouton 'modifier la couleur' et appuyez sur ENTRER")
+input()
+submit_coord = get_position()
+print(submit_coord)
+time.sleep(1)
+
+print("Maintenant, pour calibrer le bouton ok de la boite de dialogue, veuillez changer la couleur d'un pixel en cliquant sur le bouton 'modifier' sans cliquer sur la boite de dialogue après, puis recliquer sur ce programme. Mettez votre souris sur le bouton 'ok' (sans cliquer) puis appuyez sur ENTRER")
+input()
+ok_coord = get_position()
+print(ok_coord)
+time.sleep(1)
+pyautogui.moveTo(ok_coord)
+pyautogui.click()
+
+with open("ma_calibration.py", 'w') as f :
+    f.writelines(f"neutral_coord = {neutral_coord}\n")
+    f.writelines(f"edit_coord = {edit_coord}\n")
+    f.writelines(f"x_coord = {x_coord}\n")
+    f.writelines(f"y_coord = {y_coord}\n")
+    f.writelines(f"color_coord = {color_coord}\n")
+    f.writelines(f"submit_coord = {submit_coord}\n")
+    f.writelines(f"ok_coord = {ok_coord}\n")
+
+    
---
+++ painter.py
@@ -0,0 +1,137 @@
+
+import random
+import time
+from pyclick import HumanClicker
+import pyautogui
+import pytesseract
+import os
+import pyperclip
+import PIL
+from datetime import datetime  
+import sys
+
+from ma_calibration import x_coord, y_coord, color_coord, neutral_coord, edit_coord, submit_coord, ok_coord
+
+
+def setup() :
+    global neutral_coord
+    global edit_coord
+
+    pyautogui.moveTo(neutral_coord)
+    pyautogui.click()
+    time.sleep(1)
+    pyautogui.press('f5')
+    time.sleep(10)
+    pyautogui.moveTo(edit_coord)
+    pyautogui.click()
+    time.sleep(10)
+    pyautogui.moveTo(neutral_coord)
+    pyautogui.scroll(-2)
+    time.sleep(2)
+        
+def get_pixel_color(x, y) :
+    global x_coord
+    global y_coord
+    global color_coord
+    pyautogui.moveTo(x_coord)
+    pyautogui.click()
+    for _ in range(5) :
+        pyautogui.press('right')
+    for _ in range(5) :
+        pyautogui.press('backspace')
+    pyautogui.typewrite(str(x), interval=0.1)
+
+    pyautogui.moveTo(y_coord)
+    pyautogui.click()
+    for _ in range(5) :
+        pyautogui.press('right')
+    for _ in range(5) :
+        pyautogui.press('backspace')
+    pyautogui.typewrite(str(y) + "\n", interval=0.1)
+
+    pyautogui.moveTo(color_coord)
+    pyautogui.click()
+    pyautogui.hotkey('ctrl', 'q')
+    pyautogui.keyDown('ctrl')
+    pyautogui.press('c')
+    pyautogui.keyUp('ctrl')
+
+    value = pyperclip.paste()[1:]
+    return value.lower()
+
+def set_pixel_color(x, y, color) :
+    global x_coord
+    global y_coord
+    global color_coord
+    global submit_coord
+    global ok_coord
+
+    pyautogui.moveTo(x_coord)
+    pyautogui.click()
+    for _ in range(5) :
+        pyautogui.press('right')
+    for _ in range(5) :
+        pyautogui.press('backspace')
+    pyautogui.typewrite(str(x), interval=0.1)
+
+    pyautogui.moveTo(y_coord)
+    pyautogui.click()
+    for _ in range(5) :
+        pyautogui.press('right')
+    for _ in range(5) :
+        pyautogui.press('backspace')
+    pyautogui.typewrite(str(y), interval=0.1)
+
+    pyautogui.moveTo(color_coord)
+    pyautogui.click()
+    for _ in range(7) :
+        pyautogui.press('right')
+    for _ in range(7) :
+        pyautogui.press('backspace')
+    pyautogui.typewrite('#' + color, interval=0.1)
+    pyautogui.moveTo(submit_coord)
+    pyautogui.click()
+    time.sleep(3)
+    pyautogui.moveTo(ok_coord)
+    pyautogui.click()
+
+
+def rgba2hex(pixel) :
+    r, g, b, _ = pixel
+    return '{:02x}{:02x}{:02x}'.format(r, g, b)
+
+
+
+def main() :
+    # offset_x = 224
+    # offset_y = 72
+    # tardis = PIL.Image.open('tardis.png')
+
+
+    offset_x = 1
+    offset_y = 186
+    tardis = PIL.Image.open('rickroll.png')
+
+    width, height = tardis.size
+    last_update = 0
+
+
+    while True :
+        for i in range(height) :
+            for j in range(width) :
+                while time.time() - last_update < 118 :
+                    pass
+                # print(get_pixel_color(offset_x + j, offset_y + i), rgba2hex(tardis.getpixel((j, i))), get_pixel_color(offset_x + j, offset_y + i) != rgba2hex(tardis.getpixel((j, i))))
+                if get_pixel_color(offset_x + j, offset_y + i) != rgba2hex(tardis.getpixel((j, i))):
+                    sys.exit()
+                    ct = datetime.now()
+                    print(j, i, offset_x + j, offset_y + i, rgba2hex(tardis.getpixel((j, i))), f"{ct.hour}:{ct.minute}:{ct.second}", end='\n\n')
+                    set_pixel_color(offset_x + j, offset_y + i, rgba2hex(tardis.getpixel((j, i))))
+                    last_update = time.time()
+                    setup()
+
+if __name__ == '__main__' :
+    # setup()
+    main()
+            
+    
